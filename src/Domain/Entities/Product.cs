c05d0c4d0fb8be1327cdecb934edd2316e94f7f1
unknown <pedro.devtech@gmail.com>
2026-09-14 09:54:07 +0800

    Initialize Product entity

---
+++ src/Domain/Entities/Product.cs
@@ -0,0 +1,23 @@
+﻿using CopylaneSalesInventory.Domain.Common;
+
+namespace CopylaneSalesInventory.Domain.Entities
+{
+    public class Product : BaseEntityModel
+    {
+        public int Id { get; set; }
+
+        public string Sku { get; set; } = string.Empty;
+
+        public string BarCode { get; set; } = string.Empty;
+
+        public string Name { get; set; } = string.Empty;
+
+        public int CategoryId { get; set; }
+
+        public decimal UnitPrice { get; set; }
+
+        public int ReorderLevel { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
